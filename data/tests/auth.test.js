174f496232395e0c261be937db541c81f72703e4
Richard Kirk <richard.kirk790@gmail.com>
2023-11-02 14:35:34 -0400

    Add register data function and validation
    This commit adds the register user function and validation for the
    input. Unit tests added verify two throw cases and success case for
    register function.
    
    Issue: https://github.com/ulrokx/student-course-review/issues/1

--- data/tests/auth.test.js
+++ data/tests/auth.test.js
@@ -1,3 +1,88 @@
-it("should do math", () => {
-  expect(1 + 1).toBe(2);
+import { register } from "../auth.js";
+import bcrypt from "bcrypt";
+import { users } from "../../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+
+jest.mock("bcrypt", () => {
+  return {
+    hash: jest.fn(() => "hashedPassword"),
+    compare: jest.fn(() => true),
+  };
+});
+
+jest.mock("../../config/mongoCollections.js");
+
+describe("data/auth", () => {
+  describe("register", () => {
+    it("should register a user", async () => {
+      const findOne = jest.fn(() => null);
+      const insertOne = jest.fn(() => {
+        return { acknowledged: true, insertedId: new ObjectId() };
+      });
+      users.mockReturnValue({
+        findOne,
+        insertOne,
+      });
+      const params = {
+        email: "bob@email.com",
+        password: "bob123bob",
+        username: "bob123",
+      };
+      const result = await register(params);
+      expect(findOne).toHaveBeenCalledWith({ email: params.email });
+      expect(bcrypt.hash).toHaveBeenCalledWith(params.password, 10);
+      expect(insertOne).toHaveBeenCalledWith({
+        email: params.email,
+        hashedPassword: "hashedPassword",
+        username: params.username,
+        admin: false,
+      });
+      expect(result.status).toBe(200);
+      expect(result.message).toBe("User added successfully");
+      expect(result.user).not.toHaveProperty("hashedPassword");
+      expect(typeof result.user.id).toBe("string");
+    });
+
+    it("should throw an error if email is already taken", async () => {
+      const findOne = jest.fn(() => ({ email: "taken@email.com" }));
+      const insertOne = jest.fn();
+      users.mockReturnValue({ findOne, insertOne });
+      await expect(() =>
+        register({
+          email: "taken@email.com",
+          username: "taken",
+          password: "123456789",
+        }),
+      ).rejects.toHaveProperty("status", 400);
+      expect(insertOne).not.toHaveBeenCalled();
+    });
+
+    it("should throw an error if db insert fails", async () => {
+      const findOne = jest.fn(() => null);
+      const insertOne = jest.fn(() => {
+        return { acknowledged: false };
+      });
+      users.mockReturnValue({
+        findOne,
+        insertOne,
+      });
+      const params = {
+        email: "bob@email.com",
+        password: "bob123bob",
+        username: "bob123",
+      };
+      await expect(() => register(params)).rejects.toHaveProperty(
+        "status",
+        500,
+      );
+      expect(findOne).toHaveBeenCalledWith({ email: params.email });
+      expect(bcrypt.hash).toHaveBeenCalledWith(params.password, 10);
+      expect(insertOne).toHaveBeenCalledWith({
+        email: params.email,
+        hashedPassword: "hashedPassword",
+        username: params.username,
+        admin: false,
+      });
+    });
+  });
 });
---
+++ data/validation.js
@@ -0,0 +1,21 @@
+import { ObjectId } from "mongodb";
+import { z } from "zod";
+
+const usernameRegExp = new RegExp(/^[a-zA-Z0-9_]{3,16}$/);
+
+const emailSchema = z.string().trim().email();
+
+const passwordSchema = z
+  .string()
+  .min(8, { message: "Password must be at least 8 characters long" });
+
+const usernameSchema = z.string().trim().regex(usernameRegExp, {
+  message:
+    "Username must be between 3 and 16 characters long and can only contain letters, numbers, and underscores",
+});
+
+export const registerSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+  username: usernameSchema,
+});
