156f4f85f556662db088d8f54e4df1899e9bb694
coreyheckel3 <coreyheckel3@gmail.com>
2023-11-25 16:56:46 -0500

    Fixed search issues
    fixed search issues

--- data/search.js
+++ data/search.js
@@ -1,51 +1,47 @@
-import {universities} from '../config/mongoCollections.js';
-import {ObjectId} from 'mongodb';
+import { universities } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
 
 const exportedMethods = {
-async searchUniversity(search) {
-    const universityCollection = await universities()
-    const part = new RegExp(search, 'i');
-    const universitiesList = await universityCollection.find({ name: { $regex: part } }).toArray();
-    let temp = []
-      for (let i = 0; i < universitiesList.length; i++) {
-        //console.log(`i: ${i}, temp: ${temp}`);
-        const uniList = {
-          id: universitiesList[i]._id,
-          name: universitiesList[i].name
-        };
-        temp.push(uniList)
-      }
-    return temp
-},
-async getId(id) {
+  async searchUniversity(search) {
+    const universityCollection = await universities();
+    const part = new RegExp(search, "i");
+    const universitiesList = await universityCollection
+      .find({ name: { $regex: part } })
+      .toArray();
+    let temp = [];
+    for (let i = 0; i < universitiesList.length; i++) {
+      const uniList = {
+        _id: universitiesList[i]._id,
+        name: universitiesList[i].name,
+      };
+      temp.push(uniList);
+    }
+    return temp;
+  },
 
-    if (!id)
-    {
-      throw 'Error: id does not exist'
-    } 
-      if (typeof id !== 'string')
-      {
-        throw 'Error: Id must be a string'
-      } 
-      if (id.trim().length === 0)
-      {
-        throw 'Error: id is empty'
-      } 
-      id = id.trim();
-      if (!ObjectId.isValid(id))
-      {
-        throw 'Error: invalid ID'
-      } 
-      const universityCollection = await universities()
-      const university = await universityCollection.findOne({_id: new ObjectId(id)})
-      if (!university)
-      {
-        throw 'Error: No event with that id'
-      } 
-  
-      return university;
-  
-  }
+  /*async getId(id) {
+    if (!id) {
+      throw "Error: id does not exist";
+    }
+    if (typeof id !== "string") {
+      throw "Error: Id must be a string";
+    }
+    if (id.trim().length === 0) {
+      throw "Error: id is empty";
+    }
+    id = id.trim();
+    if (!ObjectId.isValid(id)) {
+      throw "Error: invalid ID";
+    }
+    const universityCollection = await universities();
+    const university = await universityCollection.findOne({
+      _id: new ObjectId(id),
+    });
+    if (!university) {
+      throw "Error: No event with that id";
+    }
 
-}
+    return university;
+  },*/
+};
 export default exportedMethods;
--- routes/search.js
+++ routes/search.js
@@ -1,81 +1,74 @@
 import { Router } from "express";
-import universityData from '../data/search.js'
-import validation from '../data/validation.js'
-import {getUniversity} from '../data/universities.js'
+import universityData from "../data/search.js";
+import validation from "../data/validation.js";
+import { getUniversity } from "../data/universities.js";
 const router = Router();
 
-router
-  .get("/", async (req, res) => {
-    try
-    {
-        res.render("search");
-    }
-    catch (e)
-    {
-        res.status(500).json({ error: e });
-    }
-  })
-  router.route('/searchresults').post(async (req, res) => {
-    //code here for POST this is where your form will be submitting searchCharacterByName and then call your data function passing in the searchCharacterByName and then rendering the search results of up to 15 characters.
-    try
-    {
-      const data = req.body;
-  
-      let search = validation.checkSearch(data.searchTerm)
-    }
-    catch (e)
-    {
-      res.status(400).render('error', {errorMessage: "Please provide a valid search term"})
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.render("search");
+  } catch (e) {
+    res
+      .status(500)
+      .render("error", { errorMessage1: "Failed to render search page" });
+  }
+});
+router.route("/searchresults").post(async (req, res) => {
+  try {
+    const data = req.body;
 
-    try
-  {
-    let search = req.body.searchTerm
-    search = search.trim()
-    const data1 = await universityData.searchUniversity(search)
-      if(data1.length == 0)
-      {
-        return res.status(404).render('error', {errorMessage1: "No results were found for " + search})
-      }
-      res.render("search-universities", {universitySearchResult: data1, search})
+    let search = validation.checkSearch(data.searchTerm);
+  } catch (e) {
+    res
+      .status(400)
+      .render("error", { errorMessage: "Please provide a valid search term" });
   }
-  catch (e)
-  {
-    console.error(e)
-    res.status(404).render('error',{errorMessage2: "Unable to gather character data"})
+
+  try {
+    let search = req.body.searchTerm;
+    search = search.trim();
+    const searchResults = await universityData.searchUniversity(search);
+    if (searchResults.length == 0) {
+      return res.status(404).render("error", {
+        errorMessage1: "No results were found for " + search,
+      });
+    }
+    res.render("search-universities", {
+      universitySearchResult: searchResults,
+      search,
+    });
+  } catch (e) {
+    console.error(e);
+    res
+      .status(404)
+      .render("error", { errorMessage2: "Unable to gather character data" });
   }
-  });
-  
-  
-  router.route('/search-universities/:id').get(async (req, res) => {
-    //code here for GET a single character
-    try
-    {
-      let search = req.params.id
-      search = validation.checkId(search)
+});
+
+router.route("/search-universities/:id").get(async (req, res) => {
+  try {
+    let search = req.params.id;
+    search = validation.checkId(search);
+  } catch (e) {
+    res
+      .status(400)
+      .render("error", { errorMessage: "Please provide a valid id" });
+  }
+  try {
+    let search = req.params.id;
+    const data = await getUniversity(search);
+    if (data.length == 0) {
+      return res.status(404).render("error", {
+        errorMessage1: "We're sorry, but no results were found for " + search,
+      });
     }
-    catch (e)
-    {
-      res.status(400).render("error", {errorMessage: "Please provide a valid id"})
-    }
-    try
-    {
-      let search = req.params.id
-      //search = validation.checkId(search)
-      const data = await universityData.getId(search)
-      if(data.length == 0)
-      {
-        
-        return res.status(404).render("error", {errorMessage1: "We're sorry, but no results were found for " + search})
-      }
-      res.render("universitiesById", {universityById: data})
-    }
-    catch (e)
-    {
-      console.log(e)
-      res.status(404).render("error",{errorMessage2: "Unable to gather character data"})
-    }
-  });
-  
-  export default router
-  
+    res.render("universities-by-id", { university: data });
+  } catch (e) {
+    console.log(e);
+    res
+      .status(404)
+      .render("error", { errorMessage2: "Unable to gather character data" });
+  }
+});
+
+export default router;
