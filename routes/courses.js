7f4ce92eec168c51f6c0226e0f3fe1c30f1d198e
Richard Kirk <richard.kirk790@gmail.com>
2023-12-07 12:39:29 -0500

    Add initial course page
    This commit adds a page for a course and makes it accessible from the
    courses list.

--- routes/courses.js
+++ routes/courses.js
@@ -1,37 +1,54 @@
 import { Router } from "express";
-import { getAllCourses, getCourses } from "../data/courses.js";
+import { getAllCourses, getCourse, getCourses } from "../data/courses.js";
 import { searchCourse } from "../data/search.js";
-import { searchCourseSchema } from "../data/validation.js";
+import { idSchema, searchCourseSchema } from "../data/validation.js";
 
 const router = Router();
 
-const formatCourses = (courses) =>
-  courses.map((course) => ({
-    ...course,
-    professors: course.professors.join(", "),
-  }));
-
-router.get("/", async (req, res) => {
-  const { search } = req.query;
-  if (!search) {
-    const courses = await getAllCourses();
-    return res.render("courses", { courses: formatCourses(courses) });
-  }
-  const parseResults = searchCourseSchema.safeParse(search);
-  if (!parseResults.success) {
-    return res
-      .status(400)
-      .json({ message: parseResults.error.issues[0].message });
-  }
-  try {
-    const courses = await searchCourse(parseResults.data);
-    res.render("courses", {
-      search,
-      courses: formatCourses(courses),
-    });
-  } catch (e) {
-    res.status(e.status).render("error", e);
-  }
+const formatCourse = (course) => ({
+  ...course,
+  professors: course.professors.join(", "),
 });
+
+const formatCourses = (courses) => courses.map(formatCourse);
+
+router
+  .get("/", async (req, res) => {
+    const { search } = req.query;
+    if (!search) {
+      const courses = await getAllCourses();
+      return res.render("courses", { courses: formatCourses(courses) });
+    }
+    const parseResults = searchCourseSchema.safeParse(search);
+    if (!parseResults.success) {
+      return res
+        .status(400)
+        .json({ message: parseResults.error.issues[0].message });
+    }
+    try {
+      const courses = await searchCourse(parseResults.data);
+      res.render("courses", {
+        search,
+        courses: formatCourses(courses),
+      });
+    } catch (e) {
+      res.status(e.status).render("error", e);
+    }
+  })
+  .get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const parseResults = idSchema.safeParse(id);
+    if (!parseResults.success) {
+      return res
+        .status(400)
+        .json({ message: parseResults.error.issues[0].message });
+    }
+    try {
+      const course = await getCourse(id);
+      res.render("course", { course: formatCourse(course) });
+    } catch (e) {
+      res.status(e.status).render("error", e);
+    }
+  });
 
 export default router;
