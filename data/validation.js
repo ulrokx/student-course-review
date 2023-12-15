edce44bee27163d831ecefc0b91fc24d0839b2bc
coreyheckel3 <coreyheckel3@gmail.com>
2023-12-14 19:03:17 -0500

    Updated passwordSchema
    Updated password Schema

--- data/validation.js
+++ data/validation.js
@@ -16,7 +16,17 @@ const emailSchema = z.string().trim().email().refine(xss);
 
 const passwordSchema = z
   .string()
-  .min(8, { message: "Password must be at least 8 characters long" });
+  .min(8, { message: "Password must be at least 8 characters long" })
+  .regex(/[a-z]/, {
+    message: "Password must contain at least one lowercase letter",
+  })
+  .regex(/[A-Z]/, {
+    message: "Password must contain at least one uppercase letter",
+  })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(/[^a-zA-Z0-9]/, {
+    message: "Password must contain at least one special character",
+  });
 
 const usernameSchema = z
   .string()
